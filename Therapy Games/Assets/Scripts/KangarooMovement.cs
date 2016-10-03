e85f4a26bfe4ced8492280d15df4251a5845e58f
Gunnar L Wambaugh <gwambaugh@unr.edu>
2016-10-03 11:51:09 -0700

    Added game folder structure

---
+++ Therapy Games/Assets/Scripts/KangarooMovement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KangarooMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
