349b2a4b530da91bcae66a8cf798560a2257edaa
Daniel Lopez <daniellopez@nevada.unr.edu>
2016-10-19 15:17:41 -0700

    Extended plotter to other waveforms encapsulated in diffreent functions that reference different global structs

--- Simulator/numPlotter.py
+++ Simulator/numPlotter.py
@@ -1,19 +1,42 @@
 import math
 import random
 import matplotlib.pyplot as plt
+import numpy
 
-typicalSwallow = []
+correctData = []
 noiseArray = []
 combined = []
 
-highAmplitude = 126     #the peak of a typical swallow was at around 37.5ish 
-                        #microvolts, therefore, the peak mut be some factor 
-                        #of that given the standard deviation
+num_samples  = 100
 
-mean = 89               #arbitrary
+typicalSwallow = {
+	"amp" : 126,		# the peak of a typical swallow was at around 37.5ish 
+                        # microvolts, therefore, the peak mut be some factor 
+                        # of that given the standard deviation
+	"mean":	89,			# arbitrary
+	"sd"  : 1.35		# arbitrary
+}
 
-standard_deviation = 1.35  #also arbitrary, makes thing easier
+effortfulSwallow = {
+	"amp" : 396,		# the peak of a typical swallow was at around 37.5ish 
+                        # microvolts, therefore, the peak mut be some factor 
+                        # of that given the standard deviation
+	"mean":	89,			# arbitrary
+	"sd"  : 1.79		# arbitrary	
+}
 
+mendelsohn = [
+	{
+		"amp" : 700,
+		"mean": 69,
+		"sd"  : 4.5,
+	},
+	{
+		"amp" : 800,
+		"mean": 76.7,
+		"sd"  : 7.8,	
+	}
+]
 
 def bellCurve(x, a, mu, sd):
 	coef = a / (sd * math.sqrt(2 * math.pi))
@@ -24,21 +47,81 @@ def bellCurve(x, a, mu, sd):
 	rhs = math.exp(rhs)
 	return coef * rhs
 
-for x in range(0,100):
-	noiseArray.append(random.uniform(1.875,7.5)) #look at video for small noise
+def sawtoothCurve():
+	pass
 
-for x in range(0,100):
-	if x > 0:
-		noiseArray[x] = noiseArray[x-1]*0.2 + noiseArray[x]*0.8
-	typicalSwallow.append(bellCurve(x, highAmplitude, mean, standard_deviation))
-	combined.append(noiseArray[x] + typicalSwallow[x])
+def populateNoise():
+	# populate the noise array
+	for x in range(0,num_samples):
+		#noiseArray.append(random.uniform(1.875,7.5)) #look at video for small noise
+		noiseArray.append(1)
+		if x > 0:
+			noiseArray[x] = noiseArray[x-1]*0.2 + noiseArray[x]*0.8	
 
-print "Pure Data   |   Noise Data   |   Result"
-for x in range(0,100):
-	print "{}   |   {}   |   {}".format(typicalSwallow[x], noiseArray[x], combined[x])
 
-plt.plot(range(0,100),combined,"b-")
+def populateTypicalSwallow():
+	populateNoise()
 
-plt.axis([0,100,0,50])
+	# populate the rest of the array
+	for x in range(0,num_samples):
+		correctData.append(bellCurve(x, typicalSwallow["amp"], typicalSwallow["mean"], typicalSwallow["sd"]))
+		combined.append(noiseArray[x] + correctData[x])
 
-plt.show()
+def populateEffortfulSwallow():
+	populateNoise()
+	
+	# populate the rest of the array
+	for x in range(0,num_samples):
+		correctData.append(bellCurve(x, effortfulSwallow["amp"], effortfulSwallow["mean"], effortfulSwallow["sd"]))
+		combined.append(noiseArray[x] + correctData[x])
+
+
+def populateMendelsohn():
+	populateNoise()
+
+	# populate the rest of the array
+	for x in range(0,num_samples):
+		value = 0
+		value = value + bellCurve(x, mendelsohn[0]["amp"], mendelsohn[0]["mean"], mendelsohn[0]["sd"])
+		value = value + bellCurve(x-6.9, mendelsohn[1]["amp"], mendelsohn[1]["mean"], mendelsohn[1]["sd"])
+		correctData.append(value)
+		combined.append(noiseArray[x] + correctData[x])
+
+#
+# print out all the data for it
+#
+
+if __name__ == '__main__':
+	#populateTypicalSwallow()
+	populateEffortfulSwallow()
+	#populateMendelsohn()
+
+	print "Pure Data   |   Noise Data   |   Result"
+	for x in range(0,num_samples):
+		print "{}   |   {}   |   {}".format(correctData[x], noiseArray[x], combined[x])
+
+	fig = plt.figure()
+	ax = fig.add_subplot(1,1,1)
+
+	line, = ax.plot(0, combined[0], 'b-')
+
+	for x in range(0, num_samples):
+		line.set_ydata(combined[x])
+		fig.canvas.draw()
+
+
+
+'''
+	# matplotlib stuff here
+		#define the axis
+	plt.axis([0,num_samples,0,100])
+
+	#plt.show()
+	for x in range(0, num_samples):
+		line.set_xdata(numpy.append(line.get_xdata(), x))
+		line.set_ydata(numpy.append(line.get_ydata(), combined[x]))
+		plt.draw()
+
+
+	#plt.plot(range(0,num_samples),combined,"b-")
+'''
