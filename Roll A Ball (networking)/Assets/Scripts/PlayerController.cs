05df2b5a75323528f0d1d5b6d575f52ab3088803
ljalmanzar <ljalmanzar@nevada.unr.edu>
2016-10-29 18:49:19 -0600

    starting networking

--- Roll A Ball (networking)/Assets/Scripts/PlayerController.cs
+++ Roll A Ball (networking)/Assets/Scripts/PlayerController.cs
@@ -1,15 +1,22 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
     public float speed;
+    public Text countText;
+    public Text winText;
 
     private Rigidbody rb;
+    private int count; 
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
     }
     void FixedUpdate ()
     {
@@ -25,6 +32,18 @@ public class PlayerController : MonoBehaviour {
         if (other.gameObject.CompareTag("Pick Up"));
         {
             other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+
+        if (count >= 11)
+        {
+            winText.text = "You Win!";
         }
     }
 }
