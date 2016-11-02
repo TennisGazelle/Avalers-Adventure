ae7d8bfc4b6b593563ab2d3d833b7102d396f89f
Gunnar L Wambaugh <gwambaugh@unr.edu>
2016-11-02 15:59:09 -0700

    Added script to recieve udp packets

---
+++ Therapy Games/Assets/Scripts/SendUDPStream.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Net;
+using System.Threading;
+
+
+public class SendUDPStream : MonoBehaviour {
+    public string IpAddress = "127.0.0.1";
+    public int PortNumber = 3000;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
