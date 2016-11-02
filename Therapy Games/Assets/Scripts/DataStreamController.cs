fba01ba5e1aaa3895b62f2aaf79deaf21cdc30e8
gunnarw <gunnar@wambaugh.org>
2016-11-02 14:30:34 -0700

    Added script for data stream controller for Unity

---
+++ Therapy Games/Assets/Scripts/DataStreamController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class DataStreamController : MonoBehaviour {
+
+	public string IpAddress = "127.0.0.1";
+	public int PortNumber = 3000;
+
+	void Start () {
+		//NetworkTransport.Init();
+	}
+
+	void Update() {
+		
+	}
+}
