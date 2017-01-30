24c00ae2e14f1633c276b86cf8ab0d73bedc85d7
gunnarW <gwambaugh@nevada.unr.edu>
2017-01-30 14:24:23 -0800

    Added outline for Graph on GUI

---
+++ FlappyBirdClone/Assets/Scripts/GraphController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class GraphController : MonoBehaviour {
+    public float maxValue = 100;
+    public float minValue = 0;
+    public float upperGoalHeight= 50;
+    public float lowerGoalHeight= 25;
+    public float backgroundHeight = 150;
+
+    public Text CurrentValueText;
+    public Text maxText;
+    public Text minText;
+
+    public GameObject background;
+    public GameObject upperGoalBound;
+    public GameObject lowerGoalBound;
+
+    private float currentValue = 0;
+
+    //Use this for initialization
+	void Awake () {
+        // init height for goal bars
+        Vector3 upperGoalPos = upperGoalBound.transform.localPosition;
+        Vector3 lowerGoalPos = lowerGoalBound.transform.localPosition;
+        upperGoalBound.transform.localPosition = new Vector3(upperGoalPos.x, upperGoalHeight - backgroundHeight, upperGoalPos.z);
+        lowerGoalBound.transform.localPosition = new Vector3(lowerGoalPos.x, lowerGoalHeight - backgroundHeight, lowerGoalPos.z);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        updateText();
+	}
+
+    void updateText()
+    {
+        // refresh texts
+        maxText.text = "Max: " + maxValue;
+        minText.text = "Min: " + minValue;
+        CurrentValueText.text = "Current Value: " + currentValue;
+    }
+}
