ba242b56b29a5eb00236d1078a5e0bbe9bf115fa
gunnarW <gwambaugh@nevada.unr.edu>
2017-02-16 19:47:59 -0800

    Initialized Theme genarator scripts

---
+++ FlappyBirdClone/Assets/Scripts/ThemeGenerator/BackgroundEntityData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BackgroundEntityData:System.Object {
+
+    public string label;
+    public GameObject backgroundObject;
+    public float initialOffset;
+    public float minSpacing;
+    public float maxSpacing;
+    public DisplayType dimensionLayout;
+}
+
---
+++ FlappyBirdClone/Assets/Scripts/ThemeGenerator/ThemeGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThemeGenerator : MonoBehaviour {
+
+    public BackgroundEntityData[] objectTypes;
+    
+    private float timer;
+
+	// Use this for initialization
+	void Awake () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+	}
+
+    
+}
