af62c09cdd39d2fd91be796d4bcfa65fd39763ea
gunnarw <gunnar@wambaugh.org>
2017-03-27 18:58:05 -0700

    Added hot air balloon to balloon game

---
+++ FlappyBirdClone/Assets/Scripts/BalloonMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        transform.RotateAround(Vector3.zero, Vector3.up, 2.5f * Time.deltaTime);
+    }
+
+
+}
--- FlappyBirdClone/Assets/Scripts/CameraFollow.cs
+++ FlappyBirdClone/Assets/Scripts/CameraFollow.cs
@@ -3,18 +3,28 @@ using System.Collections;
 
 public class CameraFollow : MonoBehaviour {
 
+    public float smoothing = 100f;
+    Vector3 offset;
 	// Use this for initialization
-	void Start () {
+	void Awake () {
         cameraZ = transform.position.z;
+        offset = transform.position - Player.position;
 	}
 
     float cameraZ;
 
 
 	void Update () {
-        transform.position = new Vector3(Player.position.x + 1.5f, 0, cameraZ);
-
-	}
+        Vector3 targetCamPos = new Vector3(
+            Player.position.x + offset.x
+            , transform.position.y
+            , Player.position.z + offset.z);
+        transform.RotateAround(Vector3.zero, Vector3.up, 2.5f * Time.deltaTime);
+        //transform.position = new Vector3(Player.position.x + 1.5f, 0, cameraZ);
+        // transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+        //float y = transform.eulerAngles.y;
+       // transform.LookAt(new Vector3(Player.transform.position.x, transform.position.y, Player.transform.position.z));
+    }
 
 
     public Transform Player;
