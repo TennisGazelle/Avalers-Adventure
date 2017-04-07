da24fc82c89c8de4518a03fb8dfdc19612b1dbec
Luis Almanzar <ljalmanzar@gmail.com>
2017-04-06 20:21:38 -0700

    Asteroid movement working

--- FlappyBirdClone/Assets/Scripts/SlingshotGame/ProjectileDragging.cs
+++ FlappyBirdClone/Assets/Scripts/SlingshotGame/ProjectileDragging.cs
@@ -5,33 +5,42 @@ using UnityEngine;
 public class ProjectileDragging : MonoBehaviour {
 
     public float maxStretch = 3.0f;
-    public LineRenderer frontCatapult;
-    public LineRenderer backCatapult;
+    //public LineRenderer frontCatapult;
+    //public LineRenderer backCatapult;
 
     private SpringJoint2D spring;
     private Rigidbody2D rb;
 
-	void Awake()
+    private float baseline = GameSettingsControl.Instance.baselineSwallow;
+    private float percentageOfBaseline = GameSettingsControl.Instance.baselinePercentage;
+
+    private float tempInput;
+    private float targetValue;
+    private Vector3 winningForce;
+
+    void Awake()
     {
         spring = GetComponent<SpringJoint2D>();
         rb = GetComponent<Rigidbody2D>();
+
+        //tempInput = 50.0f;
+        //targetValue = baseline * percentageOfBaseline * (0.01f);
+
+        tempInput = 50;
+        targetValue = 50;
+
+        winningForce = new Vector3(100, 50, 0);
     }
 	void Start () {
-        LineRendererSetup();
+        //LineRendererSetup();
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		if (spring != null)
-        {
+        // check the input from emg, if good check for success (temp for now)
 
-        }
-        else
-        {
-
-        }
+        // if input => target * percentage, add the correct force to win, if not apply the percent there
 	}
-
+/*
     void LineRendererSetup(){
         frontCatapult.SetPosition(0, frontCatapult.transform.position);
         backCatapult.SetPosition(0, backCatapult.transform.position);
@@ -42,15 +51,32 @@ public class ProjectileDragging : MonoBehaviour {
         frontCatapult.sortingOrder = 3;
         backCatapult.sortingOrder = 1;
     }
+*/
+    void OnMouseDown(){
 
-    void onSwallow(){
-        // disable spring so it can move
-        spring.enabled = false;
+        float percentageThere;
 
-        // move back based on input from external
+        if (tempInput >= targetValue)
+        {
+            // so nothing holds onto asteroid
+            Destroy(spring);
 
-        // enable spring so it can fling away; turn kinematic off so it can use physics
-        spring.enabled = true;
-        rb.bodyType = RigidbodyType2D.Dynamic;
+            // turn make dynamic so physics can work
+            rb.bodyType = RigidbodyType2D.Dynamic;
+
+            rb.AddForce(winningForce, ForceMode2D.Impulse);
+        }
+        else
+        {
+            percentageThere = tempInput / targetValue;
+
+            // so nothing holds onto asteroid
+            Destroy(spring);
+
+            // turn make dynamic so physics can work
+            rb.bodyType = RigidbodyType2D.Dynamic;
+            // add force
+            rb.AddForce(new Vector3((winningForce.x * percentageThere), winningForce.y, 0), ForceMode2D.Impulse);
+        }
     }
 }
