7fefa38ba017c7b07daeea75ed25e43b5d651e54
UNR\lalmanzar <ljalmanzar@gmial.com>
2017-04-09 13:18:29 -0700

    Adding timers

--- FlappyBirdClone/Assets/Scripts/SlingshotGame/ProjectileDragging.cs
+++ FlappyBirdClone/Assets/Scripts/SlingshotGame/ProjectileDragging.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ProjectileDragging : MonoBehaviour {
 
+    public Text timerText;
+    public Text commandText;
+    public Text swallowText;
+
     public float maxStretch = 3.0f;
-    //public LineRenderer frontCatapult;
-    //public LineRenderer backCatapult;
 
     private SpringJoint2D spring;
     private Rigidbody2D rb;
@@ -17,6 +20,12 @@ public class ProjectileDragging : MonoBehaviour {
     private float tempInput;
     private float targetValue;
     private Vector3 winningForce;
+
+    private float timeBetweenSwallows;
+    private float restTimeLeft;
+    private float swallowingWindowTimer;
+    private float swallowingWindow;
+
 
     void Awake()
     {
@@ -29,48 +38,59 @@ public class ProjectileDragging : MonoBehaviour {
         tempInput = 50;
         targetValue = 50;
 
-        winningForce = new Vector3(85, 50, 0);
+        //timeBetweenSwallows = GameSettingsControl.Instance.restDuration;
+        timeBetweenSwallows = 3.0f;
+        restTimeLeft = timeBetweenSwallows;
+
+        swallowingWindowTimer = 0.0f;
+        swallowingWindow = 3.0f;
+
+    winningForce = new Vector3(100, 40, 0);
     }
 	void Start () {
         //LineRendererSetup();
 	}
 	
 	void Update () {
+
+        timerText.text = restTimeLeft.ToString("f2");
+        swallowText.text = swallowingWindowTimer.ToString("f2");
+
+        // update timer 
+        if (restTimeLeft > 0)
+        {
+            commandText.text = "Relax";
+            restTimeLeft -= Time.deltaTime;
+            return;
+        }
+
+        if (restTimeLeft <= 0 && swallowingWindowTimer <= swallowingWindow)
+        {
+            commandText.text = "Swallow!";
+            swallowingWindowTimer += Time.deltaTime;
+            return;
+        }
+        else if (restTimeLeft <= 0 && swallowingWindowTimer >= swallowingWindow)
+        {
+            commandText.text = "Not quick enough";
+            restTimeLeft = timeBetweenSwallows;
+            //swallowingWindowTimer = 0.0f;
+            return;
+        }
+        else
+        {
+            // do nothing
+        }
+
         // check the input from emg, if good check for success (temp for now)
-
-        // if input => target * percentage, add the correct force to win, if not apply the percent there
-	}
-
-/*
-    void LineRendererSetup(){
-        frontCatapult.SetPosition(0, frontCatapult.transform.position);
-        backCatapult.SetPosition(0, backCatapult.transform.position);
-
-        frontCatapult.sortingLayerName = "SlingshotForeground";
-        backCatapult.sortingLayerName = "SlingshotForeground";
-
-        frontCatapult.sortingOrder = 3;
-        backCatapult.sortingOrder = 1;
     }
-*/
+
     void OnMouseDown(){
 
         float percentageThere;
 
-        if (tempInput >= targetValue)
+        if (restTimeLeft == 0 && swallowingWindowTimer <= swallowingWindow)
         {
-            // so nothing holds onto asteroid
-            Destroy(spring);
-
-            // turn make dynamic so physics can work
-            rb.bodyType = RigidbodyType2D.Dynamic;
-            rb.angularDrag = 3f;
-
-            rb.AddForce(winningForce, ForceMode2D.Impulse);
-        }
-        else
-        {
-            percentageThere = tempInput / targetValue;
 
             // so nothing holds onto asteroid
             Destroy(spring);
@@ -79,8 +99,21 @@ public class ProjectileDragging : MonoBehaviour {
             rb.bodyType = RigidbodyType2D.Dynamic;
             rb.angularDrag = 3f;
 
-            // add force
-            rb.AddForce(new Vector3((winningForce.x * percentageThere), winningForce.y, 0), ForceMode2D.Impulse);
+            if (tempInput >= targetValue)
+            {
+                rb.AddForce(winningForce, ForceMode2D.Impulse);
+            }
+            else
+            {
+                percentageThere = tempInput / targetValue;
+
+                // add force
+                rb.AddForce(new Vector3((winningForce.x * percentageThere), winningForce.y, 0), ForceMode2D.Impulse);
+            }
+
+            // reset timers
+            restTimeLeft = timeBetweenSwallows;
+            swallowingWindowTimer = 0.0f;
         }
     }
 }
