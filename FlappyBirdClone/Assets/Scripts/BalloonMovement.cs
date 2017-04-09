8544018cdd46853a8f73652babba44493683b63b
UNR\lalmanzar <ljalmanzar@gmial.com>
2017-04-09 10:43:59 -0700

    movement added to balloon with mouse and spacebar (have to disable BalloonBob)

--- FlappyBirdClone/Assets/Scripts/BalloonMovement.cs
+++ FlappyBirdClone/Assets/Scripts/BalloonMovement.cs
@@ -4,14 +4,28 @@ using UnityEngine;
 
 public class BalloonMovement : MonoBehaviour {
 
+    private Rigidbody rb;
+    private Vector3 winningForce; 
+
 	// Use this for initialization
 	void Awake () {
-		
+        rb = GetComponent<Rigidbody>();
+        winningForce = new Vector3(0,500,0);
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
         transform.RotateAround(Vector3.zero, Vector3.up, 2.5f * Time.deltaTime);
+
+        if (Input.GetKeyDown("space"))
+        {
+            rb.AddForce(winningForce, ForceMode.Force);
+        }
+    }
+
+    void OnMouseDown()
+    {
+        rb.AddForce(winningForce, ForceMode.Force);
     }
 
 
