a89acbe8373768100e64b230fc547aa8a4f05fd5
gunnarw <gunnar@wambaugh.org>
2017-04-12 20:45:04 -0700

    Added waypoint script

---
+++ FlappyBirdClone/Assets/Scripts/WaypointReachablePath.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointReachablePath : MonoBehaviour {
+    public Transform[] waypoints;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public Transform[] GetReachablePaths()
+    {
+        return waypoints;
+    }
+}
