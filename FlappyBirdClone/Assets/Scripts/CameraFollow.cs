3348e93574b4fab5074ac13e9cf15400c68fe999
gunnarw <gunnar@wambaugh.org>
2017-04-13 01:21:40 -0700

    Changed camera movement

--- FlappyBirdClone/Assets/Scripts/CameraFollow.cs
+++ FlappyBirdClone/Assets/Scripts/CameraFollow.cs
@@ -3,27 +3,28 @@ using System.Collections;
 
 public class CameraFollow : MonoBehaviour {
 
-    public float smoothing = 100f;
+    public float smoothing = 8f;
     Vector3 offset;
+    public Transform center;
 	// Use this for initialization
 	void Awake () {
-        cameraZ = transform.position.z;
-        offset = transform.position - Player.position;
+        offset =  transform.position - Player.position;
 	}
 
-    float cameraZ;
 
-
-	void Update () {
+	void FixedUpdate () {
         Vector3 targetCamPos = new Vector3(
-            Player.position.x + offset.x
+            Player.position.x + offset.z
             , transform.position.y
             , Player.position.z + offset.z);
-        transform.RotateAround(Vector3.zero, Vector3.up, 2.5f * Time.deltaTime);
-        //transform.position = new Vector3(Player.position.x + 1.5f, 0, cameraZ);
-        // transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+       
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
         //float y = transform.eulerAngles.y;
-       // transform.LookAt(new Vector3(Player.transform.position.x, transform.position.y, Player.transform.position.z));
+        // transform.LookAt(new Vector3(Player.transform.position.x, transform.position.y, Player.transform.position.z));
+        Vector3 lookat =  Player.transform.position - Player.GetComponent<BalloonMovement>().currentWaypoint.position;
+        lookat.Normalize();
+
+        transform.LookAt(Player.position + lookat);
     }
 
 
