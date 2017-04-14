61281a0c4fa083414321e5128149dd09747a0e09
Daniel Lopez <daniellopez@nevada.unr.edu>
2017-04-13 22:52:00 -0700

    added gold ingot stuff

---
+++ FlappyBirdClone/Assets/RotateAndBobScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAndBobScript : MonoBehaviour {
+    public float period;
+    public float amplitude;
+    public float offsetInY;
+
+    public float constantYOffset;
+
+	// Use this for initialization
+	void Start () {
+        offsetInY = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position -= new Vector3(0, offsetInY+constantYOffset, 0);
+        offsetInY = Mathf.Sin(Time.time * period) * amplitude;
+        transform.position += new Vector3(0, offsetInY+constantYOffset, 0);
+        transform.Rotate(Vector3.up, 2, Space.Self);
+	}
+
+    void OnTriggerEnter(Collider collector) {
+        ScoreManagerScript.Score++;
+        Destroy(gameObject);
+    }
+}
