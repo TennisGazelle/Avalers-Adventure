0dd4bccf1bb6ba5b342b8530f5d3e0061929bf04
Daniel Lopez <daniellopez@nevada.unr.edu>
2017-05-02 13:19:46 -0700

    finished score

---
+++ FlappyBirdClone/Assets/GameModeController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameModeController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetGameMode(GameMode mode) {
+        GameSettingsControl.Instance.mode = mode;
+    }
+
+    public void SetToTypical() {
+        SetGameMode(GameMode.Typical);
+    }
+
+    public void SetToEfforful() {
+        SetGameMode(GameMode.Effortful);
+    }
+
+    public void SetToMendelsohn() {
+        SetGameMode(GameMode.Mendelsohn);
+    }
+
+    public void SetToRandom() {
+        SetGameMode(GameMode.Random);
+    }
+}
